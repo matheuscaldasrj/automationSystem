2e6f43107a0a774af17d0f882d1d091e00dfd559
Matheus Caldas <matheuscaldasrj@gmail.com>
2018-02-05 23:47:22 -0200

    first commit

---
+++ src/main/java/com/mcaldas/automation/Application.java
@@ -0,0 +1,24 @@
+package com.mcaldas.automation;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+	/**
+	 * WAR packaging
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+	
+}
